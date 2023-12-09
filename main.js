10d3882e59536e403ab1254d929617a5e1a2cbd4
CameronHoying <143430904+CameronHoying@users.noreply.github.com>
2023-12-09 14:47:21 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+
+var h = document.getElementById('hour');
+var m = document.getElementById('minute');
+var s = document.getElementById('sec');
+
+var starttimer = null;
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59
+        m.value--;
+    } else if (h.value != 0 && m.value == 0 && s.value == 0) {
+		m.value = 59;
+		s.value = 59;
+		h.value--;
+    }
+    return;
+}
+
+function stopTimer(){
+    clearInterval(startTimer);
+}
+
+start.addEventListener('click', function(){
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval()
+})
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    stopTimer()
+})
